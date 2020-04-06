8222365b3b26947db4a664acf7d71d8ec5495b5e
Heather Swift <hswift@lincolnschool.org>
2020-04-06 14:16:47 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Heather Swift on 4/6/20.
+//  Copyright © 2020 Heather Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
